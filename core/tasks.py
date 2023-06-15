a740e2a1c8d27c86af652c77dafddf3675374e5f
Галымжан <galymzan@MacBook-Pro-Galymzhan.local>
2023-06-15 15:34:36 +0600

    Create a task to fetch and save stock data for 10 stocks; Remove api key

---
+++ core/tasks.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Stock
+from celery import shared_task
+import finnhub
+import datetime
+
+# Create your views here.
+
+# stock symbols, company profile (may get repetetive), stock quote 
+
+# Microsoft, Apple, Google, Tesla, Meta, Netflix, Amazon, Nvidia, Intel, AMD
+
+@shared_task
+def fetch_current_data():
+    finnhub_client = finnhub.Client(api_key="")
+    stocks_symbols = ['MSFT', 'AAPL', 'GOOGL', 'TSLA', 'META', 'NFLX', 'AMZN', 'NVDA', 'INTC', 'AMD']
+    for symbol in stocks_symbols: 
+        stock_quote = finnhub_client.quote(symbol) #a dictionary
+        stock = {'symbol':symbol, 'time':datetime.datetime.now()}
+        stock.update(stock_quote)
+        obj = Stock(**stock)
+        obj.save()
+
