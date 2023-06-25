d57a77534e9f80b8c03cea3b57879f02307b2443
Галымжан <galymzan@MacBook-Pro-Galymzhan.local>
2023-06-25 23:33:28 +0600

    Add JWT refresh and access token urls

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+app_name = 'api'
+
 urlpatterns = [
     path('', views.fetch_current_data, name='index'),
-    path('<str:symbol>/', views.history, name='history'),
+    path('history/<str:symbol>/', views.history, name='history'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
