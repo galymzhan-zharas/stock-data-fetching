cfc58fb91964f7a5eaa9e62e7d337ddda9f18ae9
Галымжан <galymzan@MacBook-Pro-Galymzhan.local>
2023-06-27 18:05:10 +0600

    Create a new model "Company"

--- core/models.py
+++ core/models.py
@@ -1,8 +1,28 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
 
+class Company(models.Model):
+    country = models.CharField()
+    currency = models.CharField()
+    estimateCurrency = models.CharField()
+    exchange = models.CharField()
+    ipo = models.DateTimeField()
+    marketCapitalization = models.DecimalField(max_digits=30, decimal_places=20)
+    name = models.CharField()
+    phone = models.CharField()
+    shareOutstanding = models.DecimalField(max_digits=30, decimal_places=20)
+    ticker = models.CharField()
+    weburl = models.CharField()
+    logo = models.CharField()
+    finnhubIndustry = models.CharField()
+
+    def __str__(self):
+        return self.name
+
 class Stock(models.Model):
+    company_profile = models.ForeignKey(Company, on_delete=models.CASCADE, default=None)
     symbol = models.CharField()
     c = models.DecimalField(max_digits=10, decimal_places=4)
     d = models.DecimalField(max_digits=10, decimal_places=4)
