8fda45a52e2bf2082e6b2ccbd9690d734e6fadaf
Галымжан <galymzan@MacBook-Pro-Galymzhan.local>
2023-06-27 18:04:33 +0600

    Save data inside "Company"

--- core/tasks.py
+++ core/tasks.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.conf import settings
-from .models import Stock
+from .models import Company, Stock
 from celery import shared_task
 import finnhub
 import datetime
@@ -14,11 +14,15 @@ import datetime
 
 @shared_task
 def fetch_current_data():
-    finnhub_client = finnhub.Client(api_key='chrislhr01qkb63avj10chrislhr01qkb63avj1g') #finnhub.Client(api_key=settings.FINNHUB_API_KEY)
+    finnhub_client = finnhub.Client(api_key='chrislhr01qkb63avj10chrislhr01qkb63avj1g') #finnhub.Client(api_key=settings.FINNHUB_API_KEY) 
     stocks_symbols = ['MSFT', 'AAPL', 'GOOGL', 'TSLA', 'META', 'NFLX', 'AMZN', 'NVDA', 'INTC', 'AMD']
     for symbol in stocks_symbols: 
-        stock_quote = finnhub_client.quote(symbol) #a dictionary
-        stock = {'symbol':symbol, 'time':datetime.datetime.now()}
+        company_profile = finnhub_client.company_profile2(symbol=symbol)
+        company_obj = Company(**company_profile)
+        company_obj.save()
+        
+        stock_quote = finnhub_client.quote(symbol)
+        stock = {'company_profile':company_obj, 'symbol':symbol, 'time':datetime.datetime.now()}
         stock.update(stock_quote)
         obj = Stock(**stock)
         obj.save()
