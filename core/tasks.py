8e07442153ad7211d788bdd780ab4d1203fefb1d
Галымжан <galymzan@MacBook-Pro-Galymzhan.local>
2023-06-28 18:35:29 +0600

    Use python requests instead of finnhub.api; Add response message

--- core/tasks.py
+++ core/tasks.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 from django.conf import settings
 from .models import Company, Stock
 from celery import shared_task
-import finnhub
-import datetime
+import datetime, requests
 
 # Create your views here.
 
@@ -13,17 +12,30 @@ import datetime
 # Microsoft, Apple, Google, Tesla, Meta, Netflix, Amazon, Nvidia, Intel, AMD
 
 @shared_task
-def fetch_current_data():
-    finnhub_client = finnhub.Client(api_key='chrislhr01qkb63avj10chrislhr01qkb63avj1g') #finnhub.Client(api_key=settings.FINNHUB_API_KEY) 
+def fetch_current_data(request):
+    
+    api_key='chrislhr01qkb63avj10chrislhr01qkb63avj1g' #finnhub.Client(api_key=settings.FINNHUB_API_KEY) 
+    base_url = 'https://finnhub.io/api/v1'
+    company_profile_url = f'{base_url}/stock/profile2'
+    stock_quote_url = f'{base_url}/quote'
     stocks_symbols = ['MSFT', 'AAPL', 'GOOGL', 'TSLA', 'META', 'NFLX', 'AMZN', 'NVDA', 'INTC', 'AMD']
-    for symbol in stocks_symbols: 
-        company_profile = finnhub_client.company_profile2(symbol=symbol)
-        company_obj = Company(**company_profile)
-        company_obj.save()
-        
-        stock_quote = finnhub_client.quote(symbol)
-        stock = {'company_profile':company_obj, 'symbol':symbol, 'time':datetime.datetime.now()}
-        stock.update(stock_quote)
-        obj = Stock(**stock)
-        obj.save()
+
+    for symbol in stocks_symbols:
+        try:
+            params = {'symbol':symbol, 'token':api_key}
+            company_profile = requests.get(company_profile_url, params=params).json()
+            company_obj = Company(**company_profile)
+            company_obj.save()
+
+            stock_quote = requests.get(stock_quote_url, params=params).json()
+            stock = {'company_profile': company_obj, 'symbol': symbol, 'time': datetime.datetime.now()}
+            stock.update(stock_quote)
+            stock_obj = Stock(**stock)
+            stock_obj.save()
+        except requests.exceptions.RequestException as e:
+            if e.response.status_code == 503: 
+                return HttpResponse("503: server unavailable")
+            else: 
+                return HttpResponse("Error while processing the request")
+
 
